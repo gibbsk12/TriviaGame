87c96bd5193a9fbde7f6ffed93d2a4e94db922bd
Kate Gibbs <kategibbs@Kates-MBP-2.fios-router.home>
2018-05-28 17:20:01 -0400

    created list of trivia questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,77 @@
 var triviaQuestions = [
-    {question: ""}
+    {
+        question: "What were the Washington Redskins originally called?",
+        answerChoices: ["Boston Redskins", "Boston Giants", "Boston Titans", "Boston Braves"],
+        correctAnswer: [3]
+    },
+    {
+        question: "Who was the first player ever drafted by the Redskins?",
+        answerChoices: ["Joe Stydahar", "Riley Smith", "Cliff Battles", "Sammy Baugh"],
+        correctAnswer: [1]
+    },
+    {
+        question: "Who was the first Washington Redskin player inducted into the NFL Hall of Fame?",
+        answerChoices: ["Bill Dudley", "Turk Edwards", "Cliff Battles", "Sammy Baugh"],
+        correctAnswer: [3]
+    },
+    {
+        question: "Which of the following Super Bowls did the Washington Redskins NOT win?",
+        answerChoices: ["Super Bowl XVII", "Super Bowl XVIII", "Super Bowl XXII", "Super Bowl XXVI"],
+        correctAnswer: [1]
+    },
+    {
+        question: "In what year did the Redskins move to Washington?",
+        answerChoices: ["1932", "1933", "1937", "1942"],
+        correctAnswer: [2]
+    },
+    {
+        question: "How many Super Bowls have the Washington Redskins won?",
+        answerChoices: ["1", "2", "3", "4"],
+        correctAnswer: [2]
+    },
+    {
+        question: "What is the name of the Redskins Stadium?",
+        answerChoices: ["FedEx Field", "The Verizon Center", "Lucas Oil Stadium", "MetLife Stadium"],
+        correctAnswer: [0]
+    },
+    {
+        question: "Which of the following jersey numbers is retired?",
+        answerChoices: ["3", "11", "33", "67"],
+        correctAnswer: [2]
+    },
+    {
+        question: "Who has the all-time rushing record?",
+        answerChoices: ["Larry Brown", "John Riggins", "Alfred Morris", "Ladell Betts"],
+        correctAnswer: [1]
+    },
+    {
+        question: "Who has the all-time passing record?",
+        answerChoices: ["Joe Theismann", "Sonny Jurgensen", "Mark Rypien", "Kirk Cousins"],
+        correctAnswer: [0]
+    },
+    {
+        question: "What is the name of the Redskins Fight Song?",
+        answerChoices: ["You Like That?", "Battle of the Braves!", "Honky Tonk Hogs", "Hail to the Redskins!"],
+        correctAnswer: [3]
+    },
+    {
+        question: "Who is the current owner of the Washington Redskins?",
+        answerChoices: ["Dan Snyder", "Jack Kent Cooke", "Harry Wismer", "Jerry Jones"],
+        correctAnswer: [0]
+    },
+    {
+        question: "Who is the current coach of the Washington Redskins?",
+        answerChoices: ["Joe Gibbs", "Jon Gruden", "Jay Gruden", "Kyle Shanahan"],
+        correctAnswer: [2]
+    },
+    {
+        question: "Which of the following is a Redskins color?",
+        answerChoices: ["black", "yellow", "burgandy", "white"],
+        correctAnswer: [2]
+    },
+    {
+        question: "Which team can be considered the Redskins GREATEST rival?",
+        answerChoices: ["Pittsburgh Steelers", "Dallas Cowboys", "Seattle Seahawks", "Minnesota Vikings"],
+        correctAnswer: [1]
+    },
 ]
