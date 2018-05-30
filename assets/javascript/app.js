16b86aaf9b9a0d21d5def1f2b28348899da22543
Kate Gibbs <kategibbs@Kates-MacBook-Pro-2.local>
2018-05-29 23:33:52 -0400

    fixed typos and images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,91 +10,91 @@ $(document).ready(function () {
             question: "What were the Washington Redskins originally called?",
             answerChoices: ["Boston Redskins", "Boston Giants", "Boston Titans", "Boston Braves"],
             correctAnswer: "Boston Braves",
-            image: "<img src = 'assets/images/question1.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question1.jpg' width='200px'>"
         },
         {
             question: "Who was the first player ever drafted by the Redskins?",
             answerChoices: ["Joe Stydahar", "Riley Smith", "Cliff Battles", "Sammy Baugh"],
             correctAnswer: "Riley Smith",
-            image: "<img src = 'assets/images/question2.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question2.jpg' width='200px'>"
         },
         {
             question: "Who was the first Washington Redskin player inducted into the NFL Hall of Fame?",
             answerChoices: ["Bill Dudley", "Turk Edwards", "Cliff Battles", "Sammy Baugh"],
             correctAnswer: "Sammy Baugh",
-            image: "<img src = 'assets/images/question3.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question3.jpg' width='200px'>"
         },
         {
             question: "Which of the following Super Bowls did the Washington Redskins NOT win?",
             answerChoices: ["Super Bowl XVII", "Super Bowl XVIII", "Super Bowl XXII", "Super Bowl XXVI"],
             correctAnswer: "Super Bowl XVIII",
-            image: "<img src = 'assets/images/question4.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question4.jpg' width='200px'>"
         },
         {
             question: "In what year did the Redskins move to Washington?",
             answerChoices: ["1932", "1933", "1937", "1942"],
             correctAnswer: "1937",
-            image: "<img src = 'assets/images/question5.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question5.jpg' width='200px'>"
         },
         {
             question: "How many Super Bowls have the Washington Redskins won?",
             answerChoices: ["1", "2", "3", "4"],
             correctAnswer: "3",
-            image: "<img src = 'assets/images/question6.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question6.jpg' width='200px'>"
         },
         {
             question: "What is the name of the Redskins Stadium?",
             answerChoices: ["FedEx Field", "The Verizon Center", "Lucas Oil Stadium", "MetLife Stadium"],
             correctAnswer: "FedEx Field",
-            image: "<img src = 'assets/images/question7.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question7.jpg' width='200px'>"
         },
         {
             question: "Which of the following jersey numbers is retired?",
             answerChoices: ["3", "11", "33", "67"],
             correctAnswer: "33",
-            image: "<img src = 'assets/images/question8.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question8.jpg' width='200px'>"
         },
         {
             question: "Who has the all-time rushing record?",
             answerChoices: ["Larry Brown", "John Riggins", "Alfred Morris", "Ladell Betts"],
             correctAnswer: "John Riggins",
-            image: "<img src = 'assets/images/question9.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question9.jpg' width='200px'>"
         },
         {
             question: "Who has the all-time passing record?",
             answerChoices: ["Joe Theismann", "Sonny Jurgensen", "Mark Rypien", "Kirk Cousins"],
-            correctAnswer: "BJoe Theismann",
-            image: "<img src = 'assets/images/question10.jpg' width='100px'>"
+            correctAnswer: "Joe Theismann",
+            image: "<img src = 'assets/images/question10.jpg' width='200px'>"
         },
         {
             question: "What is the name of the Redskins Fight Song?",
             answerChoices: ["You Like That?", "Battle of the Braves!", "Honky Tonk Hogs", "Hail to the Redskins!"],
             correctAnswer: "Hail to the Redskins",
-            image: "<img src = 'assets/images/question11.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question11.jpg' width='200px'>"
         },
         {
             question: "Who is the current owner of the Washington Redskins?",
             answerChoices: ["Dan Snyder", "Jack Kent Cooke", "Harry Wismer", "Jerry Jones"],
             correctAnswer: "Dan Snyder",
-            image: "<img src = 'assets/images/question12.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question12.jpg' width='200px'>"
         },
         {
             question: "Who is the current coach of the Washington Redskins?",
             answerChoices: ["Joe Gibbs", "Jon Gruden", "Jay Gruden", "Kyle Shanahan"],
             correctAnswer: "Jay Gruden",
-            image: "<img src = 'assets/images/question13.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question13.jpg' width='200px'>"
         },
         {
             question: "Which of the following is a Redskins color?",
             answerChoices: ["black", "yellow", "burgandy", "white"],
             correctAnswer: "burgandy",
-            image: "<img src = 'assets/images/question14.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question14.jpg' width='200px'>"
         },
         {
             question: "Which team can be considered the Redskins GREATEST rival?",
             answerChoices: ["Pittsburgh Steelers", "Dallas Cowboys", "Seattle Seahawks", "Minnesota Vikings"],
             correctAnswer: "Dallas Cowboys",
-            image: "<img src = 'assets/images/question15.jpg' width='100px'>"
+            image: "<img src = 'assets/images/question15.jpg' width='200px'>"
         },]
 
     function addQuestionToScreen() {
