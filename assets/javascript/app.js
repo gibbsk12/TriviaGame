31670b735fd734fa10e5177f470adc909d46377f
Kate Gibbs <kategibbs@Kates-MacBook-Pro-2.local>
2018-05-29 20:40:46 -0400

    added images and created variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -75,3 +75,123 @@ var triviaQuestions = [
         correctAnswer: [1]
     },
 ]
+var picArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10', 'question11', 'question12', 'question13', 'question14', 'question15'];
+var currentQuestion;
+var correctTotal;
+var incorrectTotal;
+var seconds;
+var answered;
+var unanswered;
+var time;
+var playerAnswer;
+var messages = {
+    correct: "Correct! You must be a true fan!",
+    incorrect: "Sorry sports fan! That's not true!",
+    endTime: "Time's Up",
+    finished: "Alright! Let's see how you did!"
+}
+
+$("startButton").on("click", function () {
+    $(this).hide();
+    newGame();
+});
+
+$("#startOverButton").on("click", function () {
+    $(this).hide();
+    newGame();
+});
+
+function newGame() {
+    $("#scoreboard").empty();
+    $("#numberCorrect").empty();
+    $("#numberIncorrect").empty();
+    currentQuestion = 0;
+    correctTotal = 0;
+    incorrectTotal = 0;
+    nextQuestion();
+}
+
+function nextQuestion() {
+    $("#responseToInput").empty();
+    $("#correctAnswer").empty();
+    $("#questionImage").empty();
+    answered = true;
+
+    $("#currentQuestion").html("Question #" + (currentQuestion + 1) + "/" + triviaQuestions.length);
+    $("#question").html("<h2>" + triviaQuestions[currentQuestion].question + "</h2>");
+    for (var i = 0; i < 4; i++) {
+        var choices = $('<div>');
+        choices.text(triviaQuestions[currentQuestion].answerChoices[i]);
+        choices.attr({ "data-index": i });
+        choices.addClass("thisChoice");
+        $(".answerChoices").append(choices);
+    }
+    countdown();
+    $(".thisChoice").on("click", function () {
+        playerAnswer = $(this).data("index");
+        clearInterval(time);
+        answerPage();
+    });
+}
+
+function countdown() {
+    seconds = 10;
+    $("#secondsRemaining").html("<h3>You have <h4>" + seconds + " </h4>seconds remaining!</h3>");
+    answered = true;
+    time = setInterval(timeOut, 1000);
+}
+
+function timeOut() {
+    seconds--;
+    $("#secondsRemaining").html("<h3>You have <h4>" + seconds + " </h4>seconds remaining!</h3>");
+    if (seconds < 1) {
+        clearInterval(time);
+        answered = false;
+        answerPage();
+    }
+}
+
+function answerPage() {
+    $("#currentQuestion").empty();
+    $(".thisChoice").empty();
+    $(".question").empty();
+
+    var rightAnswerText = triviaQuestions[currentQuestion].answerChoices[triviaQuestions[currentQuestion].answer];
+    var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+    $("#questionImage").html('<img src = "./assets/images/' + gifArray[currentQuestion] + '.gif" width = "200px">');
+    //checks to see correct, incorrect, or unanswered
+    if ((playerAnswer == rightAnswerIndex) && (answered == true)) {
+        correctTotal++;
+        $("#responsetoInput").html(messages.correct);
+    } else if ((playerAnswer != rightAnswerIndex) && (answered == true)) {
+        incorrectTotal++;
+        $('#responsetoInput').html(messages.incorrect);
+        $('#correctAnswer').html('The correct answer was: ' + rightAnswerText);
+    } else {
+        $('#responsetoInput').html(messages.endTime);
+        $('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+        answered = true;
+    }
+
+    if (currentQuestion == (triviaQuestions.length - 1)) {
+        setTimeout(scoreboard, 5000)
+    } else {
+        currentQuestion++;
+        setTimeout(newQuestion, 5000);
+    }
+}
+function scoreboard() {
+    $("secondsRemaining").empty();
+    $("#responsetoInput").empty();
+    $("#correctAnswer").empty();
+    $("#questionImage").empty();
+
+    $("#scoreboard").html(messages.finished);
+    $("#numberCorrect").html("Correct Answers: " + correctTotal);
+    $("#numberIncorrect").html("Incorrect Answers: " + incorrectTotal);
+    $("#startOverButton").addClass("reset");
+    $("#startOverButton").show();
+    $("#startOverButton").html('Play Again?');
+}
+
+
